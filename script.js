177aa045e9bd8f1a349728a99fea1d7148d69527
RafaPires-web <rafaelpires877@gmail.com>
2025-03-02 21:32:34 -0300

    Projeto-Java

---
+++ script.js
@@ -0,0 +1,53 @@
+const Convertbutton = document.querySelector("#Conversor")
+const Alteraçao = document.querySelector(".Seletor2")
+const ValordoOutros = document.querySelector(".ValorConvertido") // Valor Da Conversao
+
+
+
+function Convertendo() {
+    const InputValor = document.querySelector(".Input-Resultado").value
+
+    const ValorDolar = 5.8
+    const ValorEuro = 6.1
+
+    if (Alteraçao.value == 'Dolar') {
+        ValordoOutros.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(InputValor / ValorDolar)
+    }
+    if (Alteraçao.value == 'Euro') {
+        ValordoOutros.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(InputValor / ValorEuro)
+    }
+
+    const ValordoReal = document.querySelector(".ValorConverter") // Valor real
+
+    ValordoReal.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(InputValor)
+
+}
+function TrocaSeletor() {
+    const Band = document.getElementById("Bandeira")
+    const imagemMoeda = document.querySelector(".Moeda-Dolar")
+
+    if (Alteraçao.value == 'Dolar') {
+        Bandeira.innerHTML = "Dolar Americano"
+        imagemMoeda.src = 'assets/Dolar.png'
+    }
+
+    if (Alteraçao.value == 'Euro') {
+        Bandeira.innerHTML = "Euro"
+        imagemMoeda.src = 'assets/Euro.png'
+
+    }
+    Convertendo()
+}
+
+Alteraçao.addEventListener('change', TrocaSeletor)
+Convertbutton.addEventListener("click", Convertendo)
+
